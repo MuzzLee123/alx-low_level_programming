302f3e9a638e37c496c3a300b6900499f96ef10a
MuzzLee123 <portiakuuf@gmail.com>
2022-09-19 20:44:12 -0700

    Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+*strlen - function that returns the length of a string.
+* @s: string to check
+*
+*  *
+*
+*   * Return: Integer
+*
+*/
+int _strlen(char *s)
+{
+int counter;
+counter = 0;
+while (*(s + counter) != '\0')
+counter++;
+return (counter);
+}
